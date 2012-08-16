9001e83c4979d7470a713a58d28aa2875f62b5c9
Rodrigo <IFSP@PC12LAB3.(none)>
2012-08-16 19:59:25 -0300

    Adicionei a mensagem inicial

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -8,7 +8,9 @@ namespace Depuracao
     {
         static void Main(string[] args)
         {
-
+            int numero = 0;
+            Console.WriteLine("O número escolhido é: {0}", numero);
+            Console.Read();
         }
     }
 }
