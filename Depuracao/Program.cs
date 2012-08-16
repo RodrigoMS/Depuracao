873a99eba43f76a405ec5f1d42000fa6d24b4b79
Rodrigo <rodrigoms1@ig.com.br>
2012-08-16 20:31:53 -0300

    Adicionei o metodo mudar()

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -6,9 +6,17 @@ namespace Depuracao
 {
     class Program
     {
+        static int Mudar(int n)
+        {
+            n = n + 1;
+            return n;
+        }
         static void Main(string[] args)
         {
             int numero = 0;
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("O número escolhido é: {0}", numero);
+            numero = Mudar(numero);
             Console.WriteLine("O número escolhido é: {0}", numero);
             Console.Read();
         }
