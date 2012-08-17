67369ce34e99c17cf47c37405f26a71862be3c3a
Rodrigo <rodrigoms1@ig.com.br>
2012-08-16 22:01:27 -0300

    adicionei mais algumas coisas

--- Depuracao/Program.cs
+++ Depuracao/Program.cs
@@ -28,13 +28,22 @@ namespace Depuracao
             int denominador = 0;
             denominador = int.Parse(Console.ReadLine());
             if (denominador == 0)
-            {
                 Console.WriteLine("Não é possivel dividir {0} por zero", denominador);
-            }
             else
-            {
                 Console.WriteLine("O quociente entre {0} e {1} é {2}", numero, denominador, Dividir(numero, denominador));
-            }
+
+            int[] vetor = new int[3];
+            vetor[0]= int.Parse(Console.ReadLine());
+            vetor[1]= int.Parse(Console.ReadLine());
+            vetor[2]= int.Parse(Console.ReadLine());
+            for (int c = 0; c < vetor.Length; c++)
+                Console.Write("{0} ", vetor[c]);
+
+            // exeção muito comun que é variave com valor null
+            //string[] v = null;
+            //string N2 = "Nada" + v[0]; 
+            //Console.WriteLine(v[0]);
+
             Console.Read();
         }
     }
